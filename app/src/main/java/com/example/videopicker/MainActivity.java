4659d58990bbb3e922a3fba1f1cc707e729c8a1f
Everyday Programmer <79357167+Everyday-Programmer@users.noreply.github.com>
2023-04-22 21:04:25 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/videopicker/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.videopicker;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+    ActivityResultLauncher<PickVisualMediaRequest> launcher = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), new ActivityResultCallback<Uri>() {
+        @Override
+        public void onActivityResult(Uri o) {
+            if (o == null) {
+                Toast.makeText(MainActivity.this, "No video is selected", Toast.LENGTH_SHORT).show();
+            } else {
+                videoView.setVideoURI(o);
+                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        mediaPlayer.start();
+                    }
+                });
+            }
+        }
+    });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MaterialButton pickVideo = findViewById(R.id.pickVideo);
+        videoView = findViewById(R.id.videoView);
+
+        pickVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launcher.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.VideoOnly.INSTANCE)
+                        .build());
+            }
+        });
+    }
+}
